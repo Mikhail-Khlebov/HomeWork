4fca2c65e12fbdc0103730b0716d8c7913725fcf
Mikhail_Kl <277572@mail.ru>
2023-05-28 00:20:46 +0300

    сохранение Задачи №13(коррект.)

--- Sem_2/Program.cs
+++ Sem_2/Program.cs
@@ -13,21 +13,15 @@
 // 		Console.WriteLine("Не трехзначное число");
 //     }
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-Console.Clear();
-Console.Write("Введите минимум 3-х значное число:");
-int Value = int.Parse(Console.ReadLine()!);
-	Console.Write("Число содержит менее 3-х цифр.\nВведите число:");
-    Value = int.Parse(Console.ReadLine()!);
-int Length = Value.ToString().Length;
-if (Length >= 3) 
-{
-	while (Value > 999)
-	{
-		Value = Value / 10;
-	}
-	int result = Value % 10;
-	Console.WriteLine($"Третья цифра: {result}");
-    } 
-    else 
-    {
-}
+// Console.Clear();
+// Console.Write("Введите минимум 3-х значное число:");
+// int Value = int.Parse(Console.ReadLine()!);
+// 	Console.Write("Число содержит менее 3-х цифр.\nВведите число:");
+//     Value = int.Parse(Console.ReadLine()!);
+// int Length = Value.ToString().Length;
+// if (Length >= 3 && Value > 999) 
+// 	{
+// 	Value = Value / 10;
+// 	}
+// 	int result = Value % 10;
+// 	Console.WriteLine($"Третья цифра: {result}");
