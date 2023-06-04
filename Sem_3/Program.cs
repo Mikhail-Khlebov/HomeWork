2f7454232f6e84583dedc602259375325ed81aff
Mikhail_Kl <277572@mail.ru>
2023-06-04 11:18:27 +0300

    выполнил задачи 21 и 23

--- Sem_3/Program.cs
+++ Sem_3/Program.cs
@@ -37,17 +37,47 @@
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, 
 // является ли оно палиндромом.
 
+// Console.Clear();
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine()!;
+// Console.Write($"Введи правильное число\nВведите число: ");
+// 	number = Console.ReadLine()!;
+// int len = number.Length;
+// if (len == 5)
+// {
+//   if (number[0]==number[4] || number[1]==number[3])
+//   {
+//     Console.WriteLine($"Ваше число: {number} - палиндром.");
+//   }
+//   else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+// }
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Console.Clear();
+// Console.Write("Введите координату X(1): ");
+// double x1 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату Y(1): ");
+// double y1 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату Z(1): ");
+// double z1 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату X(2): ");
+// double x2 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату Y(2): ");
+// double y2 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату Z(2): ");
+// double z2 = Double.Parse(Console.ReadLine()!);
+
+// double A = x2 - x1;
+// double B = y2 - y1;
+// double C = z2 - z1;
+
+// double S = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"Расстояние равно {Math.Round(S, 3)}");
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Console.Clear();
 Console.Write("Введите число: ");
-string number = Console.ReadLine()!;
-Console.Write($"Введи правильное число\nВведите число: ");
-	number = Console.ReadLine()!;
-int len = number.Length;
-if (len == 5)
-{
-  if (number[0]==number[4] || number[1]==number[3])
-  {
-    Console.WriteLine($"Ваше число: {number} - палиндром.");
-  }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
-}
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n);
+for (int i = 1; i <= n; i++)
+	Console.Write($"{i * i * i} ");
