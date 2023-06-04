98018fe11c35d6c1181f5f48f31e0d526ccd021a
Mikhail_Kl <277572@mail.ru>
2023-06-04 10:30:13 +0300

    выполнил задачу 19

--- Sem_3/Program.cs
+++ Sem_3/Program.cs
@@ -14,17 +14,40 @@
 //     Console.WriteLine("x < 0 && y < 0");
 // else
 //     Console.WriteLine("x > 0 && y < 0");
+// Console.Clear();
+// Console.Write("Введите координату X(1): ");
+// double x1 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату Y(1): ");
+// double y1 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату X(2): ");
+// double x2 = Double.Parse(Console.ReadLine()!);
+// Console.Write("Введите координату Y(2): ");
+// double y2 = Double.Parse(Console.ReadLine()!);
+// double S = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y2 - y1, 2));
+// Console.WriteLine($"Расстояние равно {Math.Round(S, 3)}");
 
-Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// n = Math.Abs(n);
+// for (int i = 1; i <= n; i++)
+// 	Console.Write($"{i * i} ");
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
 Console.Clear();
-Console.Write("Введите минимум 3-х значное число:");
-int Value = int.Parse(Console.ReadLine()!);
-	Console.Write("Число содержит менее 3-х цифр.\nВведите число:");
-    Value = int.Parse(Console.ReadLine()!);
-int Length = Value.ToString().Length;
-if (Length >= 3 && Value > 999) 
-	{
-	Value = Value / 10;
-	}
-	int result = Value % 10;
-	Console.WriteLine($"Третья цифра: {result}");
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+Console.Write($"Введи правильное число\nВведите число: ");
+	number = Console.ReadLine()!;
+int len = number.Length;
+if (len == 5)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
