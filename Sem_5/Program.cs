7c42bd7cca0f4eeda068dea96fbcd078c7960728
277572@mail.ru <277572@mail.ru>
2023-06-28 17:36:21 +0500

    38 задача решение

--- Sem_5/Program.cs
+++ Sem_5/Program.cs
@@ -65,28 +65,51 @@
 // }
 // Console.Write(sumNumbersEvenIndex);
 
-void InputArray(int [] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(-50, 51);
-}
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+// минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+// int size = 10;
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// int max = numbers[0];
+// int min = numbers[0];
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > max)
+//     {
+//         max = numbers[i];
+//     }
+//     else if (numbers[i] < min)
+//     {
+//         min = numbers[i];
+//     }
+// }
 
 
-String SearchNumbers(int[] array, int number)
-{
-    foreach (int element in array)
-    {
-        if (element == number)
-        return "yes";
-    }
-    return "no";
-}
-Console.Clear();
-Console.WriteLine("Ввидите кол-во элементов: ");
-int n = int.Parse(Console.ReadLine()!);
-int [] array = new int[n];
-InputArray(array);
-Console.WriteLine($"Массив: [{string.Join( ", ", array)}]");
-Console.Write($"Ввидите чесло которое хатите найти:");
-int k = int.Parse(Console.ReadLine()!);
-Console.Write(SearchNumbers(array, k));
+
+
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 555);
+//     }
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine($"Минимальное число: {min}");
+// Console.WriteLine($"Минимальное число: {max}");
+// Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
