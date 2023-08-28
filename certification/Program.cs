d9a6b961f2df5d12d205444165fc0bf2b9789537
277572@mail.ru <277572@mail.ru>
2023-08-28 22:22:49 +0500

    сохранил ветку хоумворк

--- certification/Program.cs
+++ certification/Program.cs
@@ -4,60 +4,60 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Clear();
-int Prompt(string message)
-{
-  Console.Write(message);
-  int result = Convert.ToInt32(Console.ReadLine());
-  return result;
-}
+// Console.Clear();
+// int Prompt(string message)
+// {
+//   Console.Write(message);
+//   int result = Convert.ToInt32(Console.ReadLine());
+//   return result;
+// }
 
-int NaturalNumber(int n, int m)
-{
-  if (n == m) return n;
-  else Console.Write($"{NaturalNumber(n, m + 1)}, ");
-  return m;
-}
+// int NaturalNumber(int n, int m)
+// {
+//   if (n == m) return n;
+//   else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+//   return m;
+// }
 
-int n = Prompt("Input N: ");
-if (n < 1)
-{
-  Console.WriteLine("Ввдите положительное число!");
-  return;
-}
-Console.WriteLine(NaturalNumber(n, 1));
+// int n = Prompt("Input N: ");
+// if (n < 1)
+// {
+//   Console.WriteLine("Ввдите положительное число!");
+//   return;
+// }
+// Console.WriteLine(NaturalNumber(n, 1));
 
 //  Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 //  M = 1; N = 15 -> 120
 //  M = 4; N = 8. -> 30
  
-Console.Write("Введите число M: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-SumFromMToN(m, n);
+// SumFromMToN(m, n);
 
-// вызов функции "сумма чисел от M до N"
-void SumFromMToN(int m, int n)
-{
-    Console.Write(SumMN(m - 1, n));
-}
+// // вызов функции "сумма чисел от M до N"
+// void SumFromMToN(int m, int n)
+// {
+//     Console.Write(SumMN(m - 1, n));
+// }
 
-// функция сумма чисел от M до N
-int SumMN(int m, int n)
-{
-    int res = m;
-    if (m == n)
-        return 0;
-    else
-    {
-        m++;
-        res = m + SumMN(m, n);
-        return res;
-    }
-}
+// // функция сумма чисел от M до N
+// int SumMN(int m, int n)
+// {
+//     int res = m;
+//     if (m == n)
+//         return 0;
+//     else
+//     {
+//         m++;
+//         res = m + SumMN(m, n);
+//         return res;
+//     }
+// }
 
 /*
  Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
@@ -65,34 +65,34 @@ int SumMN(int m, int n)
  m = 3, n = 2 -> A(m,n) = 29
  */
  
-Console.Write("Введите число M: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-AkkermanFunction(m,n);
+// AkkermanFunction(m,n);
 
 
-// вызов функции Аккермана
-void AkkermanFunction(int m, int n)
-{
-    Console.Write(Akkerman(m, n)); 
-}
+// // вызов функции Аккермана
+// void AkkermanFunction(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n)); 
+// }
 
-// функция Аккермана
-int Akkerman(int m, int n)
-{
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (n == 0 && m > 0)
-    {
-        return Akkerman(m - 1, 1);
-    }
-    else
-    {
-        return (Akkerman(m - 1, Akkerman(m, n - 1)));
-    }
-}
+// // функция Аккермана
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
